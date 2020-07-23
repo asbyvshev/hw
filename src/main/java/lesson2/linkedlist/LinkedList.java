1127b63da96227334aae8d35e76c9551740aa657
asbyvshev <barseto4ka@live.ru>
2020-07-23 17:04:14 +0300

    Добавлен интерфейс связного списка

---
+++ src/main/java/lesson2/linkedlist/LinkedList.java
@@ -0,0 +1,27 @@
+package lesson2.linkedlist;
+
+import lesson2.ICollection;
+
+public interface LinkedList<E> extends ICollection {
+    void insertFirst(E value);
+
+    E removeFirst();
+
+    boolean remove(E value);
+
+    boolean contains(E value);
+
+    void display();
+
+    E getFirstValue();
+
+
+    class Entry<T> {
+        public final T value;
+        public Entry<T> next;
+
+        public Entry(T value) {
+            this.value = value;
+        }
+    }
+}
